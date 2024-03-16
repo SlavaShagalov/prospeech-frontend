5008ce8a1abed4ddef1766d619b253c9f88c14da
Shagalov Viacheslav <shagalovva@student.bmstu.ru>
2024-03-16 16:30:43 +0300

    Added text for audio.

--- src/models/Audio.ts
+++ src/models/Audio.ts
@@ -1,10 +1,11 @@
 type Audio = {
-  id: bigint,
-  user_id: bigint,
-  title: string,
-  url: string,
-  created_at: Date,
-  updated_at: Date,
+  id: bigint;
+  user_id: bigint;
+  title: string;
+  url: string;
+  text: string;
+  created_at: Date;
+  updated_at: Date;
 };
 
 export default Audio;
